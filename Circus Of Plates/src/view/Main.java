bd597d67bc285ee545741b4689b0bf8c266d6155
Mohamed Ayman <mystic_sorcerer@hotmail.com>
2016-12-28 01:23:42 +0200

    Remove unneeded imports

--- Circus Of Plates/src/view/Main.java
+++ Circus Of Plates/src/view/Main.java
@@ -3,9 +3,6 @@ package view;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
