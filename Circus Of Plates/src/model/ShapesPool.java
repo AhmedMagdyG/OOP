376fbb1edcaaa7d5f8c56192931277fc4312b25e
Mahmoud Hussein <Mahmoud Hussein@DESKTOP-SQR5QU5>
2017-01-25 17:14:38 +0200

    ya rb

--- Circus Of Plates/src/model/ShapesPool.java
+++ Circus Of Plates/src/model/ShapesPool.java
@@ -13,27 +13,29 @@ public class ShapesPool {
 	private List<CustomShape> available;
 	private List<CustomShape> inUse;
 	private ShapeGenerator shapeGenerator;
-
 	private static ShapesPool shapesPool;
-	
+	private static int createdShapes;
+
 	private ShapesPool() {
+		createdShapes = 0;
 		available = new ArrayList<CustomShape>();
 		inUse = new ArrayList<CustomShape>();
 		shapeGenerator = new ShapeGenerator();
 	}
 
-	public static ShapesPool getInstance(){
-		if(shapesPool == null){
+	public static ShapesPool getInstance() {
+		if (shapesPool == null) {
 			shapesPool = new ShapesPool();
 		}
 		return shapesPool;
 	}
-	
+
 	public CustomShape getObject() {
 		CustomShape generatedShape = null;
-		if (available.size() + inUse.size() < POOL_SIZE) {
+		if (createdShapes < POOL_SIZE) {
 			generatedShape = shapeGenerator.getShape();
 			inUse.add(generatedShape);
+			createdShapes++;
 		} else if (!available.isEmpty()) {
 			generatedShape = available.get(new Random().nextInt(available.size()));
 			useShape(generatedShape);
@@ -41,16 +43,14 @@ public class ShapesPool {
 		return generatedShape;
 	}
 
-	public boolean releaseShape(CustomShape expiredShape) {
+	public void releaseShape(CustomShape expiredShape) {
 		for (int index = 0; index < inUse.size(); index++) {
 			if (inUse.get(index) == expiredShape) {
-				available.add(inUse.get(index));
 				inUse.remove(index--);
-				expiredShape = null;
-				return true;
+				break;
 			}
 		}
-		return false;
+		available.add(expiredShape);
 	}
 
 	private boolean useShape(CustomShape availableShape) {
@@ -70,15 +70,16 @@ public class ShapesPool {
 	}
 
 	public void removeInUse(CustomShape shape) {
-		for(int i = 0; i < inUse.size(); i++){
-			if(inUse.get(i) == shape){
+		for (int i = 0; i < inUse.size(); i++) {
+			if (inUse.get(i) == shape) {
 				inUse.remove(i);
+				return;
 			}
 		}
 	}
 
 	public void releaseAll() {
-		while(inUse.size() > 0){
+		while (inUse.size() > 0) {
 			releaseShape(inUse.get(0));
 		}
 	}
